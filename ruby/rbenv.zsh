9b8f6ced9d3ef0cf5e17d300b09b5d1ee5f9b052
Joe Juzl <joe.juzl@bambooloans.com>
2017-11-10 20:25:07 +0100

    new laptop

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,2 +1,3 @@
 export RBENV_SHELL=zsh
+eval "$(rbenv init -)"
 command rbenv rehash 2>/dev/null
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,1 +1,4 @@
+chsh -s $(which zsh)
+
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+
