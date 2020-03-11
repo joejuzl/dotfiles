3dbd51878ad1c3e4d380668288e375781694c07d
Joseph Juzl <joejuzl@gmail.com>
2020-03-11 17:41:05 +0100

    k8 aliases

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,6 +1,19 @@
 alias ku=kubectl
+alias kup="kubectl --namespace=prod"
+alias kut="kubectl --namespace=test"
+alias kue="kubectl --namespace=eval"
+alias kum="kubectl --namespace=medtronic"
+
+alias kw="watch -n 0.5 kubectl get po"
+alias kwp="kw --namespace=prod"
+alias kwt="kw --namespace=test"
+alias kwe="kw --namespace=eval"
+alias kwm="kw --namespace=medtronic"
+
 alias kp=kpoof
 alias ke=kex
 alias kc=kubectx
 alias kn=kubens
 
+alias kcs="kc gke_io-xbird-situations_europe-west1-c_situations"
+alias kcm="kc gke_io-xbird-situations-medtronic_us-east1-d_situations"
